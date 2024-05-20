1a5fc9acd8bd380ef7e3c516d296f47d9523b54a
jackalshorns <jason@uxisfine.com>
2024-05-19 20:08:38 -0500

    Day21 - Add Food!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,40 +8,19 @@ screen.bgcolor('black')
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
+snake = Snake()
 
-for position in starting_positions:
-    new_segment = Turtle()
-    new_segment.speed("slowest")
-    new_segment.shape("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
 
 game_is_on = True
-
 while game_is_on:
     screen.update()
-    time.sleep(.1)
+    time.sleep(0.1)
 
-    for seg_num in range(len(segments) -1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].fd(20)
-
-
-
-
-
-
-
-
-
-
-
-
+    snake.move()
 
 screen.exitonclick()
